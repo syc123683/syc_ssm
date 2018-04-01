ebb867af1b72e377a50b13eb69944825376bc4fc
root <admin@example.com>
2018-04-01 16:55:30 +0800

    genarater files

---
+++ src/main/java/com/syc/ssm/dao/SysRoleMapper.java
@@ -0,0 +1,22 @@
+package com.syc.ssm.dao;
+
+import com.syc.ssm.domain.SysRole;
+import com.syc.ssm.domain.SysRoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysRoleMapper {
+    long countByExample(SysRoleExample example);
+
+    int deleteByExample(SysRoleExample example);
+
+    int insert(SysRole record);
+
+    int insertSelective(SysRole record);
+
+    List<SysRole> selectByExample(SysRoleExample example);
+
+    int updateByExampleSelective(@Param("record") SysRole record, @Param("example") SysRoleExample example);
+
+    int updateByExample(@Param("record") SysRole record, @Param("example") SysRoleExample example);
+}
---
+++ src/main/java/com/syc/ssm/domain/SysUser.java
@@ -0,0 +1,63 @@
+package com.syc.ssm.domain;
+
+public class SysUser {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Integer age;
+
+    private String password;
+
+    private Integer roleId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+}
